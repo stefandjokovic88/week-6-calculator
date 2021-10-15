547e780166771d1f5b00a66842e3058b3ac7be3b
unknown <stefan.djokovic88@gmail.com>
2021-10-15 10:14:19 +0200

    change eval func, create funcs for nums and operations, onkeydown funcs

--- script.js
+++ script.js
@@ -88,156 +88,383 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // calculator
     const input = document.getElementById("input");
+    const prev = document.getElementById("previous-operand");
+    const curr = document.getElementById("current-operand");
     
 
     // reset input
     const reset = document.getElementById("reset");
 
     function resetFunction() {
-        input.value = "";
+        curr.innerHTML = "";
+        prev.innerHTML = "";
     }
     
-    reset.addEventListener("click", resetFunction)
+    reset.addEventListener("click", resetFunction);
 
 
     // delete last character
     const del = document.getElementById("delete");
 
     function deleteLastChar() {
-        let str = input.value;
-        str = str.substring(0, str.length-1);
-        input.value = str;
+        // let str = input.value;
+        // str = str.substring(0, str.length-1);
+        // input.value = str;
+        if (curr.innerHTML != "") {
+            let str = curr.innerHTML;
+            str = str.substring(0, str.length-1);
+            curr.innerHTML = str;
+        }
+        else {
+            let str = prev.innerHTML;
+            str = str.substring(0, str.length-1);
+            prev.innerHTML = str;
+        }
     }
 
     del.addEventListener("click", deleteLastChar);
 
 
     // add character in input area on button click
-    const one = document.getElementById("one");
-    const two = document.getElementById("two");
-    const three = document.getElementById("three");
-    const four = document.getElementById("four");
-    const five = document.getElementById("five");
-    const six = document.getElementById("six");
-    const seven = document.getElementById("seven");
-    const eight = document.getElementById("eight");
-    const nine = document.getElementById("nine");
-    const zero = document.getElementById("zero");
-    const point = document.getElementById("point");
-    const addition = document.getElementById("addition");
-    const substraction = document.getElementById("substraction");
-    const division = document.getElementById("division");
-    const multiplication = document.getElementById("multiplication");
-    const equally = document.getElementById("equally");
+    // const one = document.getElementById("one");
+    // const two = document.getElementById("two");
+    // const three = document.getElementById("three");
+    // const four = document.getElementById("four");
+    // const five = document.getElementById("five");
+    // const six = document.getElementById("six");
+    // const seven = document.getElementById("seven");
+    // const eight = document.getElementById("eight");
+    // const nine = document.getElementById("nine");
+    // const zero = document.getElementById("zero");
+    // const point = document.getElementById("point");
+    // const addition = document.getElementById("addition");
+    // const substraction = document.getElementById("substraction");
+    // const division = document.getElementById("division");
+    // const multiplication = document.getElementById("multiplication");
+    // const equally = document.getElementById("equally");
 
-    function writeOne() {
-        input.value += "1";
-    }
+
     
-    one.addEventListener("click", writeOne);
 
-    function writeTwo() {
-        input.value += "2";
-    }
+
+    // function writeOne() {
+    //     curr.innerHTML += "1";
+    // }
     
-    two.addEventListener("click", writeTwo);
+    // one.addEventListener("click", writeOne);
 
-    function writeThree() {
-        input.value += "3";
-    }
+    // function writeTwo() {
+    //     curr.innerHTML += "2";
+    // }
     
-    three.addEventListener("click", writeThree);
+    // two.addEventListener("click", writeTwo);
 
-    function writeFour() {
-        input.value += "4";
-    }
+    // function writeThree() {
+    //     input.value += "3";
+    // }
     
-    four.addEventListener("click", writeFour);
+    // three.addEventListener("click", writeThree);
 
-    function writeFive() {
-        input.value += "5";
-    }
+    // function writeFour() {
+    //     input.value += "4";
+    // }
     
-    five.addEventListener("click", writeFive);
+    // four.addEventListener("click", writeFour);
 
-    function writeSix() {
-        input.value += "6";
-    }
+    // function writeFive() {
+    //     input.value += "5";
+    // }
     
-    six.addEventListener("click", writeSix);
+    // five.addEventListener("click", writeFive);
 
-    function writeSeven() {
-        input.value += "7";
-    }
+    // function writeSix() {
+    //     input.value += "6";
+    // }
     
-    seven.addEventListener("click", writeSeven);
+    // six.addEventListener("click", writeSix);
 
-    function writeEight() {
-        input.value += "8";
-    }
+    // function writeSeven() {
+    //     input.value += "7";
+    // }
     
-    eight.addEventListener("click", writeEight);
+    // seven.addEventListener("click", writeSeven);
 
-    function writeNine() {
-        input.value += "9";
-    }
+    // function writeEight() {
+    //     input.value += "8";
+    // }
     
-    nine.addEventListener("click", writeNine);
+    // eight.addEventListener("click", writeEight);
 
-    function writeZero() {
-        input.value += "0";
+    // function writeNine() {
+    //     input.value += "9";
+    // }
+    
+    // nine.addEventListener("click", writeNine);
+
+    // function writeZero() {
+    //     input.value += "0";
+    // }
+
+    // zero.addEventListener("click", writeZero);
+    // substraction.addEventListener("click", writeSubstraction);
+
+    // function writeMultiplication() {
+    //     input.value += "*";
+    // }
+
+    // multiplication.addEventListener("click", writeMultiplication);
+
+    // function writeDivision() {
+    //     input.value += "/";
+    // }
+
+    // division.addEventListener("click", writeDivision);
+
+    // function writePoint() {
+    //     input.value += ".";
+    // }
+
+    // point.addEventListener("click", writePoint);
+
+
+
+    // function writeAddition() {
+
+    //     if (curr.innerHTML != "" && prev.innerHTML != "") {
+    //         result ();
+    //         curr.innerHTML += "+";
+    //         prev.innerHTML = curr.innerHTML;
+    //         curr.innerHTML = "";
+    //     }
+    //     else {
+    //     curr.innerHTML += "+";
+    //     prev.innerHTML = curr.innerHTML;
+        
+    //     curr.innerHTML = "";
+    //     }
+
+    // }
+
+    // addition.addEventListener("click", writeAddition);
+
+
+    // function writeSubstraction() {
+
+    //     if (curr.innerHTML != "" && prev.innerHTML != "") {
+    //         result ();
+    //         curr.innerHTML += "-";
+    //         prev.innerHTML = curr.innerHTML;
+    //         curr.innerHTML = "";
+    //     }
+    //     else {
+    //     curr.innerHTML += "-";
+    //     prev.innerHTML = curr.innerHTML;
+        
+    //     curr.innerHTML = "";
+    //     }
+    // }
+
+    // substraction.addEventListener("click", writeSubstraction);
+
+    // function writeMultiplication() {
+
+    //     if (curr.innerHTML != "" && prev.innerHTML != "") {
+    //         result ();
+    //         curr.innerHTML += "x";
+    //         prev.innerHTML = curr.innerHTML;
+    //         curr.innerHTML = "";
+    //     }
+    //     else {
+    //     curr.innerHTML += "x";
+    //     prev.innerHTML = curr.innerHTML;
+        
+    //     curr.innerHTML = "";
+    //     }
+    // }
+
+    // multiplication.addEventListener("click", writeMultiplication);
+
+    // function writeDivision() {
+
+    //     if (curr.innerHTML != "" && prev.innerHTML != "") {
+    //         result ();
+    //         curr.innerHTML += "/";
+    //         prev.innerHTML = curr.innerHTML;
+    //         curr.innerHTML = "";
+    //     }
+    //     else {
+    //     curr.innerHTML += "/";
+    //     prev.innerHTML = curr.innerHTML;
+        
+    //     curr.innerHTML = "";
+    //     }
+    // }
+
+    // division.addEventListener("click", writeOperation);
+
+    
+//add operation - if we have both numbers, first call result function and then add operation on result
+    function writeOperation(oper) {
+        
+        if (curr.innerHTML != "" && prev.innerHTML != "") {
+            result ();
+            curr.innerHTML += oper;
+            prev.innerHTML = curr.innerHTML;
+            curr.innerHTML = "";
+        }
+        else {
+        curr.innerHTML += oper;
+        prev.innerHTML = curr.innerHTML;
+        
+        curr.innerHTML = "";
+        }
     }
 
-    zero.addEventListener("click", writeZero);
 
-    function writeAddition() {
-        input.value += "+";
+    const operation = document.querySelectorAll(".operation");
+
+    operation.forEach(button => {
+            button.addEventListener('click', () => {
+              writeOperation(button.textContent);
+            })
+          })
+
+    
+//add number    
+    function writeNumber(num) {
+        curr.innerHTML += num;
     }
 
-    addition.addEventListener("click", writeAddition);
+    const number = document.querySelectorAll('.char');
 
-    function writeSubstraction() {
-        input.value += "-";
-    }
+    number.forEach(button => {
+            button.addEventListener('click', () => {
+              writeNumber(button.textContent);
+            })
+          })
 
-    substraction.addEventListener("click", writeSubstraction);
 
-    function writeMultiplication() {
-        input.value += "*";
-    }
-
-    multiplication.addEventListener("click", writeMultiplication);
-
-    function writeDivision() {
-        input.value += "/";
-    }
-
-    division.addEventListener("click", writeDivision);
-
-    function writePoint() {
-        input.value += ".";
-    }
-
-    point.addEventListener("click", writePoint);
 
 
 //get result
     
-    function result() {
-        input.value = eval(input.value);
-    }
+    // function result() {
+    //     input.value = eval(input.value);
+    // }
 
-    equally.addEventListener("click", result);
+    // equally.addEventListener("click", result);
 
     
-//get result on enter key
+// get result on enter key
 
-    function onEnter(e) {
-    if (e.key === "Enter") {  
-      result();
-        }
+    // function onEnter(e) {
+    // if (e.keyCode === 13) {  
+    //   result();
+    //     }
+    // }
+
+
+// window.addEventListener("keydown", onEnter);
+
+// get result on enter key
+window.addEventListener("keydown", e =>{
+    if(e.keyCode === 13){
+        result();
+    }
+})
+
+// delete last char on backspace
+window.addEventListener("keydown", e =>{
+    if(e.keyCode === 8){
+        deleteLastChar();
+    }
+})
+
+// reset on del key
+window.addEventListener("keydown", e => {
+    if (e.keyCode === 46) {
+        resetFunction();
+    }
+})
+
+// add chars using keyboard
+window.addEventListener("keydown", e =>{
+    if((e.keyCode >= 48 && e.keyCode <= 55) || (e.keyCode == 57) || (e.keyCode >= 96 && e.keyCode <= 105) || (e.keyCode >= 107 && e.keyCode <= 111) || (e.keyCode >= 187 && e.keyCode <= 191)) {
+        curr.innerHTML += e.key;
+    }
+})
+
+// show * as x
+window.addEventListener("keydown", e =>{
+    if(e.keyCode == 56 || e.keyCode == 106) {
+        curr.innerHTML += "x";
+    }
+})
+
+// add operation using keyboard - if we have both numbers, first call result function and then add operation on result
+window.addEventListener("keydown", e =>{
+    if ((e.keyCode == 107 || e.keyCode == 187) || (e.keyCode == 109 || e.keyCode == 189) 
+        || (e.keyCode == 56 || e.keyCode == 106) || (e.keyCode == 111 || e.keyCode == 191)) {
+
+        if (curr.innerHTML != "" && prev.innerHTML != "") {
+                result ();
+                curr.innerHTML += e.key;
+                prev.innerHTML = curr.innerHTML;
+                curr.innerHTML = "";
+            }
+            else {
+                prev.innerHTML = curr.innerHTML;
+                curr.innerHTML = "";
+            }
     }
 
-   input.addEventListener("keydown", onEnter);
+        // if ((e.keyCode == 109 || e.keyCode == 189)) {
+        //     if (curr.innerHTML != "" && prev.innerHTML != "") {
+        //         result ();
+        //         curr.innerHTML += "-";
+        //         prev.innerHTML = curr.innerHTML;
+        //         curr.innerHTML = "";
+        //     }
+        //     else {
+        //     // curr.innerHTML += "-";
+        //     prev.innerHTML = curr.innerHTML;
+        //     curr.innerHTML = "";
+        //     }
+        // }
+})
+
+
+// get result
+function result() {
+    x = curr.innerHTML;
+    xx = parseFloat(x);
+    
+    y = prev.innerHTML.substring(0,prev.innerHTML.length-1);
+    yy = parseFloat(y);
+
+    o = prev.innerHTML.slice(-1);
+
+    // if ((isNaN(xx) == false) && (isNaN(yy) == false)) {
+
+        switch(o) {
+            case '+': curr.innerHTML = yy+xx;
+                      prev.innerHTML = "";
+                      break;
+            case '-': curr.innerHTML = yy-xx;
+                      prev.innerHTML = "";
+                      break;
+            case 'x': curr.innerHTML = yy*xx;
+                      prev.innerHTML = "";
+                      break;
+            case '/': curr.innerHTML = yy/xx;
+                      prev.innerHTML = "";
+                      break;
+        }
+
+    // }
+}
+
+equally.addEventListener("click", result);
+
 
 });
